1a2d581f6965f912760587a02b6aa7a66f84ee34
felixschwebel <schwebel.felix@gmail.com>
2022-11-14 21:15:32 +0100

    Show example matrix, convert user input into row/column indices

--- board.py
+++ board.py
@@ -1,9 +1,26 @@
+import copy
 class Board:
     def __init__(self):
         self.matrix = []
         for i in range(3):
             row = [' ', '  |  ', ' ', '  |  ', ' ']
             self.matrix.append(row)
+
+    def board_explanation(self):
+        #Gets a copy of the fieldmatrix
+        example_matrix = copy.deepcopy(self.matrix)
+        #Fills in the numbers for the spaces that the users have to choose from
+        field_number = 1
+        for row in example_matrix:
+            for i in range(0, 5, 2):
+                row[i] = str(field_number)
+                field_number += 1
+        #Show the example matrix to the users
+        print('INFO: To put a stone on a field, choose one of the following numbers.')
+        print('-----------------')
+        for row in example_matrix:
+            print('  ' + ''.join(row))
+            print('-----------------')
 
     def show_board(self):
         print('-----------------')
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from board import Board
 
 
+def multiplayer(matrix):
+    field = int(input('Please choose a field on the board!\n'))
+    #Converts the user input into the list entry location
+    row = round(((field+1)/3))-1
+    column = ((field+2)%3)*2
+
+
 def game():
     new_board = Board()
     print('*******************')
     print('*** TIC TAC TOE ***')
-    print('*******************\n')
-    new_board.show_board()
+    print('*******************')
+    new_board.board_explanation()
+
+    #Let's the user choose the game-mode
+    # singleplayer = input('Do you want to play against the Computer? (y/n)\n').lower()
+    # if singleplayer == 'y':
+    #     #Creates an AI-Player
+    #     pass
+    # else:
+    #     pass
+
+    multiplayer()
+
 
 
 game()
